c330512a889024f8d526596e273106c9672ceca0
Jexeones24 <jexeones@gmail.com>
2017-06-17 10:28:24 -0400

    add new_or_find_by_name to player class

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,19 +3,27 @@ class Player
 
   @@all = []
 
-
   def initialize(name="player")
     @name = name
     @wins = 0
-
     @@all << self
   end
 
   #TODO:
-  def new_or_find_by_name(name)
+  def self.new_or_find_by_name(name)
     # makes new player if they dont exist
-    #returns that player if they do
+    # returns that player if they do
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
+  def self.find_by_name(name)
+    @@all.find { |player| player.name == name }
+  end
+
+  def self.create_by_name(name)
+    new_player = Player.new
+    new_player.name=(name)
+    new_player
+  end
 
 end
