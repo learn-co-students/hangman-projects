0edeb78499894311fbd1308aacb9546d2ef1b561
github username <github email address>
2017-06-17 13:19:07 -0400

    wrote gameover method and refactored dashes to be instance variable instead of class variable

--- app/models/hangman.rb
+++ app/models/hangman.rb
@@ -6,7 +6,7 @@ class Hangman
   @@player2_name = nil
 
   @@number_of_limbs = 0
-
+  @@guessed_letters = []
   @@HANGMAN_STRINGS = [
 "    _________
    |
@@ -58,13 +58,14 @@ class Hangman
    |
 ",
   ]
-  attr_accessor :players, :number_oflimbs, :dashes, :secret_word
-  attr_reader :letter
+  attr_accessor :players, :number_of_limbs, :dashes, :secret_word
+  attr_reader :letter, :guessed_letters
 
   def initialize()
     ###This is not a good practice...
     #TODO: switch these method calls to find_by_name
     ###PLAYER NEEDS METHOD NEW OR FIND BY NAME!!!!!
+    @number_of_limbs = 0
     @player1 = Player.new(@@player1_name)
     @player2 = Player.new(@@player2_name)
   end
@@ -86,8 +87,9 @@ class Hangman
     until self.game_over? do
       input = self.get_input
       self.check_input(input)
-      binding.pry
+      #binding.pry
       self.draws_hangman_and_dashes
+      #binding.pry
     end
     self.end_game
   end
@@ -100,24 +102,24 @@ class Hangman
     #set secret_word to instance variable so can access in check_input
     @secret_word = STDIN.noecho(&:gets)
     self.make_dashes(secret_word)
+    #binding.pry
   end
 
   def make_dashes(secret_word)
     #draws dashes
     #sets secret_word equal to @@dashes
-    @@dashes = secret_word
-    dashes = ""
-    secret_word.size.times do
-    dashes += "_ "
+    @dashes = ""
+    @secret_word.size.times do
+    @dashes += "_ "
     end
-    puts dashes
+    puts @dashes
   end
 
   def draws_hangman_and_dashes  ##draws hangman and dashes
-    stuff = @@HANGMAN_STRINGS[@@number_of_limbs]
+    hangman = @@HANGMAN_STRINGS[@@number_of_limbs]
     #binding.pry
-    puts stuff #when do we set number_of_limbs
-    self.make_dashes(@@dashes)
+    puts hangman #when do we set number_of_limbs
+    self.make_dashes(@secret_word)
     #binding.pry
   end
 
@@ -127,24 +129,35 @@ class Hangman
     puts "Please enter a letter."
     letter = gets.chomp
     @input = letter
-    binding.pry
+    #binding.pry
   end
 
   def check_input(input)
     ##check to see if the input is equal to a character in the secret_word
-    @secret_word.chars.include?(@input) ? true : false
-    binding.pry
+    @secret_word.chars.include?(@input) ? self.correct_guess(input) : self.incorrect_guess
+    #binding.pry
   end
 
-  def incorrect_guess
-    #subracts limb
+
+  def incorrect_guess(input)
+    #adds limb
     @number_of_limbs += 1
+    self.display_guessed_letters
   end
 
-  def correct_guess
-    #inserts letter into proper dash index(es)
+  def correct_guess(input)#inserts letter into dash index(es)
+    word = @secret_word.chars
+    index_num = []
+    word.each_with_index { |value, idx| index_num << idx if value == input }
+    binding.pry
+    index = index_num.join().to_i
+    @dashes.insert(index, input).join(", ")
+    self.display_guessed_letters
   end
 
+  def display_guessed_letters
+    @guessed_letters
+  end
 
   def game_over?
     ##Checks to see if the game has been won by player2
@@ -152,11 +165,12 @@ class Hangman
     ##check to see how many limbs are hanging
     ## return true if game over
 
-    if @dashes == @secret_word || number_of_limbs >=
+    dashes_without_spaces = @dashes.gsub(/\s+/, "")
 
+    if dashes_without_spaces == @secret_word || @number_of_limbs > 5
+      return true
     else
       return false
-
     end
 
   end
@@ -165,6 +179,8 @@ class Hangman
     #increment either players win count
     #how will we choose which player's win count to increment?
     #self.players[]
+
+
 
   end
 
