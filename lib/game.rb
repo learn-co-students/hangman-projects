7b384b35059b7178c8dc8ecc74e85e42e275bf21
cheungth25 <cheungth25@gmail.com>
2017-06-19 17:30:30 -0400

    fixed functionality for entering the same bad quess

--- lib/game.rb
+++ lib/game.rb
@@ -72,7 +72,7 @@ def start_game(player_name)
   player = Player.find_or_create(player_name)
   word = Word.new
   check_letters = false
-  last_letter = ""
+  wrong_letters = []
   hang_man = 0
   forfeit = false
   hint = ""
@@ -95,10 +95,11 @@ def start_game(player_name)
       # !!! pass guess to word instance
       word.guess(letter)
       # !!! if bad guess then hang_man += 1
-      if word.guesses.detect { |guess| guess[:character] == letter} != nil
+      if wrong_letters.detect { |bad_letter| bad_letter == letter} != nil
         puts "You've already tried that, please try again"
       elsif word.guesses.last[:valid] == false
         hang_man += 1
+        wrong_letters << letter
       end
       # !!! if all letters are guess && hang_man < 6 then game is won
       check_letters = word.letters.all? {|letter| letter.visible == true}
