ec324755681988c7983511107a757724affaabf3
wbdana <william.b.dana@gmail.com>
2017-06-19 18:01:37 -0400

    fixed play again?

--- lib/game.rb
+++ lib/game.rb
@@ -8,7 +8,7 @@ class Game
 
   @@all = []
 
-  attr_accessor :display_array, :exit_game, :status, :users, :outcome
+  attr_accessor :display_array, :exit_game, :status, :users, :outcome, :user
   attr_reader :user_input, :incorrect_guesses, :word, :letter_array, :display
 
   def initialize
@@ -36,10 +36,20 @@ class Game
   # until game.hanged? == true; break if game.won? == true
   def play
     puts "Can you save yourself from the gallows pole?"
-    puts "Please enter your user name:"
+    puts "Please enter your user name, or type 'GAMES' to view your history:"
     user_name = gets.upcase.chomp
-    User.find_by_name(user_name) ? user = User.find_by_name(user_name) : user = User.new(user_name)
-    user.games << self
+    if user_name == "GAMES"
+      User.all.each do |user|
+        user.games.each do |game|
+          puts "#{user.name} -- #{game.status}"
+        end
+      end
+      self.play_again?
+    else
+      User.find_by_name(user_name) ? user = User.find_by_name(user_name) : user = User.new(user_name)
+      user.games << self
+      self.user = user
+    end
     until self.hanged? == true || self.won? == true || self.exit_game == true
       display.gallows
       turn = Turn.new(self.word, display, self)
@@ -76,6 +86,7 @@ class Game
       new_game.play
     else
       puts "Game over."
+      abort
     end
   end
 
